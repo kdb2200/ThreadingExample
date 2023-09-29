d5c6cc60c056d61cb2197832397a8a64c5c79740
Kyle Bauer <kbau121@seas.upenn.edu>
2023-09-28 23:33:23 -0400

    Added async example

---
+++ TheadingExample/AsyncExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Runner().Wait();
+
+            Console.WriteLine("\nPRESS ANY KEY TO EXIT");
+            Console.ReadKey();
+        }
+
+        public static async Task Runner()
+        {
+            Task parallelTask = ParallelFunction();
+
+            Console.WriteLine("I'M DOING OTHER STUFF IN PARALLEL");
+
+            await parallelTask;
+        }
+
+        public static async Task ParallelFunction()
+        {
+            Console.WriteLine("ParallelFunction Started");
+
+            await Task.Run(() => { Thread.Sleep(1000); });
+
+            Console.WriteLine("ParallelFunction Finished");
+        }
+    }
+}
